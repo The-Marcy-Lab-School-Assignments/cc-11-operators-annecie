d2e278452c4f4ca6e542f27a0222e009369bdab5
annecie <ajbexcelsior@gmail.com>
2021-11-02 15:03:25 +0000

    completed

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 //1
-function areBothSame(){
-  
+function areBothSame(num1, num2) {
+    //give function parameters.
+    //if/else statement.
+    if (num1 == num2);
+    return true
+}else{
+    return false
+}
+console.log(areBothSame(true,1));
+//2
+function areBothEqual(num1, num2) {
+    //give function parameters.
+    //if/else statement.
+    if (num1 === num2);
+    return true
+}else{
+    return false
 }
 
-//2
-function areBothEqual(){
-  
-}
 
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(num1, num2, num3, num4) {
+    if(num1 === num2 && num1 === num3 && num1 === num4)
 }
-
+ console.log(areAllFourEqual( true, true, 1 , 1))
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame() {
+
 }
 
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual() {
+
 }
-
-
